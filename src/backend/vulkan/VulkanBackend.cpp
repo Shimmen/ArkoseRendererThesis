c94fd6934608d37862f545beae29b226898dd69f
Simon Moos <simon.moos95@gmail.com>
2020-04-23 17:25:25 +0200

    Fix issue where the GUI clears before rendering

--- src/backend/vulkan/VulkanBackend.cpp
+++ src/backend/vulkan/VulkanBackend.cpp
@@ -402,11 +402,11 @@ void VulkanBackend::setupDearImgui()
     VkAttachmentDescription colorAttachment = {};
     colorAttachment.format = m_swapchainImageFormat;
     colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
-    colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_LOAD;
     colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
     colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
     colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-    colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED; // TODO: Should it really be undefined here?
+    colorAttachment.initialLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
     colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
 
     VkAttachmentReference colorAttachmentRef = {};
